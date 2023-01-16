cf66eb3cdb279a1bf3223bbf8ac4f0218f84fa27
benVmeta3 <ben@vmeta3.com>
2023-01-16 11:30:13 +0800

    fix(vip): deploy vip add transferOwnership

--- deploy/Investment/001_deploy_vip.ts
+++ deploy/Investment/001_deploy_vip.ts
@@ -16,13 +16,20 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
 
     hre.deployments.log(`contract Vip deployed at ${Vip.address}`);
 
-    await deployProxy(hre, LogicName, ProxyName);
+    const proxy = await hre.deployments.getOrNull(ProxyName);
+    if (!proxy) {
+        await deployProxy(hre, LogicName, ProxyName);
+    } else {
+        hre.deployments.log(`reusing "${ProxyName}" at ${proxy.address}`);
+        hre.deployments.log(`contract ${ProxyName} deployed at ${proxy.address}`);
+    }
 };
 
 const deployProxy = async function (hre: HardhatRuntimeEnvironment, LogicName: string, ProxyName: string) {
     const { log, getExtendedArtifact, save } = hre.deployments;
     const { owner, Administrator1, Administrator2 } = await hre.getNamedAccounts();
     const Owners = [Administrator1, Administrator2, owner];
+    const targetOwners = ['0xfeaD27a71FDA8458d8b9f9055B50800eCbCaA10e', '0x2Fe8D2Bc3FD37cD7AcbbE668A7a12F957e79D708'];
     const SignRequired = 2;
 
     const Logic = await hre.ethers.getContractFactory(LogicName);
@@ -37,6 +44,14 @@ const deployProxy = async function (hre: HardhatRuntimeEnvironment, LogicName: s
     };
     await save(ProxyName, proxyDeployments);
 
+    const Vip = <Vip>Proxy;
+    const Admin1 = await setupUser(Administrator1, { Vip })
+    const Admin2 = await setupUser(Administrator2, { Vip })
+
+    // Transfer permissions
+    await Admin1.Vip.transferOwnership(targetOwners[0]);
+    await Admin2.Vip.transferOwnership(targetOwners[1]);
+    log(`Permissions are transferred to `, targetOwners[0], targetOwners[1]);
 };
 
 export default func;
