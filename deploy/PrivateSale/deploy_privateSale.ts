96037bdf921f0be3737f45410e5836d2a1e3cc64
vmeta3-jing <jing@vmeta3.com>
2023-02-14 23:13:55 +0800

    feat(privateSale):add diamond deploy script

--- deploy/PrivateSale/deploy_privateSale.ts
+++ deploy/PrivateSale/deploy_privateSale.ts
@@ -5,14 +5,23 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const {deployments, getNamedAccounts} = hre;
   const {diamond} = deployments;
 
-  const {deployer, owner} = await getNamedAccounts();
+  const {deployer, owner, Administrator1, Administrator2} = await getNamedAccounts();
 
   await diamond.deploy('PrivateSaleDiamond', {
     from: deployer,
     owner: owner,
     facets: ['PrivateSale'],
+    execute: {
+      methodName: '__PrivateSale_init',
+      args: [
+        [Administrator1, Administrator2],
+        2,
+        '0xFC04fA99eBdd826788EE98062d607b31C8069029',
+        '0x35375c3636eaaef31a63fdd9a1b16f911d67d5b5',
+      ],
+    },
     log: true,
   });
 };
 export default func;
-func.tags = ['PrivateSaleDiamond', 'PrivateSaleDiamond_deploy'];
+func.tags = ['PrivateSaleDiamond'];
