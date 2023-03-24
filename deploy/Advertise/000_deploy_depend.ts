13096043d7953c7853a5c186abb6c06ddfe1adf4
Cody <cody@vmeta3.com>
2023-03-24 14:37:03 +0800

    audit(all): first batch audit
    - Abstract
    - Advertise
    - VM3 Token
    - Land
    - Lib

--- deploy/Advertise/000_deploy_depend.ts
+++ deploy/Advertise/000_deploy_depend.ts
@@ -6,16 +6,6 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const {deploy, log} = deployments;
 
   const {deployer} = await getNamedAccounts();
-
-  if (!hre.network.live) {
-    const TToken = await deploy('TestERC20', {
-      from: deployer,
-      log: true,
-      autoMine: true, // speed up deployment on local network (ganache, hardhat), no effect on live networks
-    });
-    log(`contract TToken deployed at ${TToken.address} using`);
-  }
-
   const Time = await deploy('Time', {
     from: deployer,
     log: true,
